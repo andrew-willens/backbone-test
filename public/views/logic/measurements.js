426d17189f1687d7378a225fdc08edccbfacba63
Andrew Willens <andrew.willens@gmail.com>
2015-05-10 20:50:24 -0400

    changed template rendering method. templates now stored on DOM

--- public/views/logic/measurements.js
+++ public/views/logic/measurements.js
@@ -2,41 +2,29 @@
 
 var Measurement = Backbone.View.extend({
     tagName: 'table',
-    className: 'sizing-table',
-    template: undefined,
+    el: "#sizingtable-container",
 
     initialize: function() {
         console.log('Measurement view initialized');
-        this.setTemplate();
-    },
-
-    setTemplate: function() {
-        var that = this;
-        $.get('views/tpl/measurement_td.html', function(html) {
-            html = html.replace(/\n/g, "")
-
-            // Compile the template
-            var template = Handlebars.compile( JSON.stringify(html) );
-
-            // Load model data into template
-            var meas_units = ['length', 'depth', 'diameter', 'height'],
-                str = "";
-            for (var i = 0; i < meas_units.length; i++) {
-                var title = meas_units[i].charAt(0).toUpperCase() + meas_units[i].slice(1);
-                var tpl = template({title: title });
-                str += (i+1) % 2 === 0 ? tpl + "</tr>" : "<tr>" + tpl;
-            };
-            
-            // Load the compiled HTML into the Backbone "el"
-            that.$el.append(str);
-
-        }).done(function() {
-            $('#sizing-table-li').html(that.render());
-        });
+        // this.setTemplate('views/tpl/measurement_td.html');
+        this.render();
     },
 
     render: function() {
-        // console.log(this.el);
+        var meas_units = ['length', 'depth', 'diameter', 'height'],
+                str = "";
+
+        for (var i = 0; i < meas_units.length; i++) {
+            var tpl = _.template( $("#sizingtable-tpl").html() );
+            
+            var title = meas_units[i].charAt(0).toUpperCase() + meas_units[i].slice(1);
+
+            tpl = tpl({title: title });
+
+            str += (i+1) % 2 === 0 ? tpl + "</tr>" : "<tr>" + tpl;
+        };
+
+        this.$el.append(str);
         return this.el;
     }
 });
